db2fec96bc3d7833f7102fdf31b518e4c0db8f6c
Den2856 <saakand77@gmail.com>
2025-08-23 19:46:58 +0300

    first commit

--- client/src/components/home/ChargersSection.tsx
+++ client/src/components/home/ChargersSection.tsx
@@ -41,7 +41,7 @@ export default function ChargersSection(): JSX.Element {
             <div key={ch._id} className="space-y-4">
               <div className="overflow-hidden">
                 <img
-                  src={`src/assets/chargers/${ch.imageUrl}`}
+                  src={`/src/assets/chargers/${ch.imageUrl}`}
                   alt={ch.title}
                   className="w-full h-full object-cover"
                 />
